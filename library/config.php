0904db00e53b8e71ec2e3003b09437fadc78adae
kuriyantoadi <melatikuning212@gmail.com>
2018-09-27 23:29:33 +0700

    update 27 sep 2018, update baru lagi

--- library/config.php
+++ library/config.php
@@ -1,17 +1,17 @@
 <?php
-// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
-$host	= "localhost";
-$user	= "root";
-$pass	= "";
+// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting
+$host	= "192.168.10.100";
+$user	= "usercbt";
+$pass	= "123";
 $db	= "dbcbt";
 
-//Menggunakan objek mysqli untuk membuat koneksi dan menyimpanya dalam variabel $mysqli	
+//Menggunakan objek mysqli untuk membuat koneksi dan menyimpanya dalam variabel $mysqli
 $mysqli = new mysqli($host, $user, $pass, $db);
 
 //Membuat variabel yang menyimpan url website dan folder website
 $url_website = "http://localhost/cbt";
 $folder_website = "/cbt";
 
-//Menentukan timezone 
-date_default_timezone_set('Asia/Jakarta'); 
+//Menentukan timezone
+date_default_timezone_set('Asia/Jakarta');
 ?>
